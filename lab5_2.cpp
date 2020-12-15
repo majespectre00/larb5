47b038c1dbb201b393bfa3a539767932becdebd5
majespectre00 <oat601_12@hotmail.com>
2020-12-15 16:29:05 +0700

    lab5_2.cpp completed

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+double deg2rad (double i1){
+    return i1*M_PI/180;
+}
+double rad2deg (double i2){
+    return i2*180/M_PI;
+}
+double findXComponent(double l1, double l2, double a1, double a2){
+    double x, y;
+    x = l1*cos(a1);
+    y = l2*cos(a2);
+    return x+y;   
+}
+double findYComponent(double l1, double l2, double a1, double a2){
+    double x, y;
+    x = l1*sin(a1);
+    y = l2*sin(a2);
+    return x+y;    
+}
+double pythagoras(double xcomp, double ycomp){
+    return sqrt(pow(xcomp,2)+pow(ycomp,2));
+}
+void showResult (double r1, double r2){
+    cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = "<<r1<<endl<<"Direction of the resultant vector (deg) = "<<r2<<endl;
+    cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
