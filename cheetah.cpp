d8c0b5bba66bace873e0e70368d889a7aa1ec4d8
majespectre00 <oat601_12@hotmail.com>
2020-12-21 23:13:55 +0700

    cheetah completed

---
+++ cheetah.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int cheetah (int money){
+    int x , y;
+    x = money/10;
+    y=x;
+    while (y >=3){
+        x += y/3;
+        y = y/3 + y%3;
+    }
+    return x;
+}
+int main(){
+    cout << cheetah(20)<<endl;
+    cout << cheetah(35)<<endl;
+    cout << cheetah(101)<<endl;
+    cout << cheetah(150)<<endl;
+    cout << cheetah(204)<<endl;
+
+}
